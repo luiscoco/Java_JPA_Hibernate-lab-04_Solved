68e516ccc5045b2ac8c7699c9e0f99d041b2b6bf
Luis Coco Enríquez <LEnriquez@luxoft.com>
2025-11-07 12:06:30 +0100

    first commit

---
+++ src/main/java/edu/jpa/TABLE_PER_CLASS/entity/Person.java
@@ -0,0 +1,30 @@
+package edu.jpa.TABLE_PER_CLASS.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Person {
+    @Id
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/edu/jpa/TABLE_PER_SUBCLASS/entity/Person.java
@@ -0,0 +1,30 @@
+package edu.jpa.TABLE_PER_SUBCLASS.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Person {
+    @Id
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
